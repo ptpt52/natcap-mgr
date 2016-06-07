527c581d4e2516deee4b5ee4ae019e99a7f38b34
Chen Minqiang <ptpt52@gmail.com>
2016-06-07 22:38:27 +0800

    update

--- vclist.watch.sh
+++ vclist.watch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+test -f /root/vclist && ipset restore -f /root/vclist
+
 while :; do
 	sleep 60
 
@@ -8,6 +10,7 @@ while :; do
 	/usr/bin/wget https://raw.githubusercontent.com/ptpt52/natcap-mgr/master/vclist -O /tmp/vclist.tmp
 	diff /tmp/vclist /tmp/vclist.tmp || {
 		sh /tmp/vclist.tmp && mv /tmp/vclist.tmp /tmp/vclist
+		ipset save vclist >/root/vclist
 	}
 
 	sleep 239
